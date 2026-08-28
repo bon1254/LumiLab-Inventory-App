88a80d45512b6a74bb5ac31c1c1b0f7afc7bc375
Jimmy Kuo <110597613+bon1254@users.noreply.github.com>
2026-08-28 15:01:55 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -63,8 +63,14 @@ def get_system_settings(sh):
     SETTING_SHEET_NAME = "⚙️系統設定"
     worksheets = sh.worksheets()
 
-    # 幫你打好的超大包資料
-    default_items = ["雷射筆", "光學鏡片", "透鏡", "濾光片", "感測器", "電源線", "馬達", "螺絲", "其他"]
+    # 幫你把截圖裡的品項全部塞進來了！
+    default_items = [
+        "雷射筆", "光學鏡片", "透鏡", "濾光片", "感測器", "電源線", "馬達", "螺絲", "其他",
+        "擴大機", "HiFi Steaeo Karaoke擴大機", "喇叭", "數位回音立體聲擴大機", 
+        "Professional Karaoke 擴大機", "Stereo Karaoke擴大機", "單片式畫框喇叭",
+        "訊號延伸器", "變壓器", "85吋電視", "55吋電視", 
+        "43型4K低藍光HDR智慧聯網顯示器", "40型低藍光LED顯示器", "32吋 HD 液晶顯示器"
+    ]
     default_brands = ["無品牌或未知", "Acer 宏碁", "ASUS 華碩", "Digital Projection 數字投影科技", "Dotation 達道", "Genuine 捷元", "i-COOLTW", "JY 聚奕工業", "LITEON 光寶科技", "Logitech 羅技", "MSI 微星", "TPLink 普聯技術", "Zyxel 兆勤科技", "DIVA 惠威", "JCT", "JWE", "LINDY 林帝", "DAYEN 大影", "MACHI 金典範", "B&W 寶華韋健", "TiKAudio 翊景", "EPSON 愛普森", "ATEN 宏正自動科技", "Litemax 晶達光電", "SAMPO 聲寶", "CHIMEI 奇美", "Haier 海爾", "UniSync 優尼森克", "WAVEST 威士波", "POLYWELL 寶利威爾", "Kolin 歌林", "Mayka 明家瑋崙", "WEITIEN 威電", "JODEWAY 久威电子", "IKEA 宜家家居", "BARCORNA", "LEKO 格雷"]
     default_areas = ["無存放區域", "1號航空箱", "2號航空箱", "3號航空箱", "4號航空箱", "5號航空箱", "吊架航空箱", "機房"]
     default_locs = ["Elaine保管中", "光敘所倉庫", "翡冷翠倉庫", "高雄駁二P3_世界界古文明展場", "台北科教館_比薩大學動物展"]
