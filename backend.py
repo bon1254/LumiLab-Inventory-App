d8c56b8f8f7419517f50431ed47e825d6457853a
Jimmy Kuo <110597613+bon1254@users.noreply.github.com>
2026-09-02 15:56:23 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -59,7 +59,7 @@ def get_system_settings(sh):
     worksheets = sh.worksheets()
 
     default_items = [
-        "雷射筆", "光學鏡片", "透鏡", "濾光片", "感測器", "電源線", "馬達", "螺絲", "其他",
+        "投影機", "投影機鏡頭", "HDMI線", "HDMI to DP線", "HDMI to DP轉接頭", "電源線", "螢幕", "鍵盤", "滑鼠",
         "擴大機", "HiFi Steaeo Karaoke擴大機", "喇叭", "數位回音立體聲擴大機", 
         "Professional Karaoke 擴大機", "Stereo Karaoke擴大機", "單片式畫框喇叭",
         "訊號延伸器", "變壓器", "85吋電視", "55吋電視", 
@@ -68,7 +68,7 @@ def get_system_settings(sh):
     default_brands = ["無品牌或未知", "Acer 宏碁", "ASUS 華碩", "Digital Projection 數字投影科技", "Dotation 達道", "Genuine 捷元", "i-COOLTW", "JY 聚奕工業", "LITEON 光寶科技", "Logitech 羅技", "MSI 微星", "TPLink 普聯技術", "Zyxel 兆勤科技", "DIVA 惠威", "JCT", "JWE", "LINDY 林帝", "DAYEN 大影", "MACHI 金典範", "B&W 寶華韋健", "TiKAudio 翊景", "EPSON 愛普森", "ATEN 宏正自動科技", "Litemax 晶達光電", "SAMPO 聲寶", "CHIMEI 奇美", "Haier 海爾", "UniSync 優尼森克", "WAVEST 威士波", "POLYWELL 寶利威爾", "Kolin 歌林", "Mayka 明家瑋崙", "WEITIEN 威電", "JODEWAY 久威电子", "IKEA 宜家家居", "BARCORNA", "LEKO 格雷"]
     default_areas = ["無存放區域", "1號航空箱", "2號航空箱", "3號航空箱", "4號航空箱", "5號航空箱", "吊架航空箱", "機房"]
     default_locs = ["Elaine保管中", "光敘所倉庫", "翡冷翠倉庫", "高雄駁二P3_世界界古文明展場", "台北科教館_比薩大學動物展"]
-    default_statuses = ["✅ 在庫", "故障中等維修", "維修中", "無法使用", "使用中", "無使用", "非案子外借中", "已賣出", "翡冷翠保管"]
+    default_statuses = ["故障中等維修", "維修中", "無法使用", "使用中", "無使用", "非案子外借中", "已賣出", "翡冷翠保管"]
 
     if SETTING_SHEET_NAME not in [ws.title for ws in worksheets]:
         mlen = max(len(default_items), len(default_brands), len(default_areas), len(default_locs), len(default_statuses))
